be59da324f45fadc41c89bec3f8143bcd8730b04
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-05-04 20:19:07 +0200

    Make a photo, save it and show it

--- app/src/main/java/com/example/driverchecker/MainActivity.kt
+++ app/src/main/java/com/example/driverchecker/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.driverchecker
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.content.ContentValues
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var buttonView: Button
     private lateinit var buttonChoose: Button
     private lateinit var imageView: ImageView
-    private lateinit var mCurrentPhotoPath: String
+    private lateinit var filePhotoUri: Uri
     private val FILENAME = "driver_checker.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,12 +39,31 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.txtView)
         buttonView = findViewById(R.id.btnView)
         buttonChoose = findViewById(R.id.btnChoose)
+        imageView = findViewById(R.id.imgView)
 
         buttonView.setOnClickListener {
-            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            filePhoto = this.getPhotoFile(FILENAME)
-            val providerFile = FileProvider.getUriForFile(this,"com.example.driverchecker.fileprovider", filePhoto)
-            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+                if (checkSelfPermission(CAMERA) == PackageManager.PERMISSION_DENIED){
+                    val permissions = arrayOf(CAMERA)
+                    requestPermissions(permissions, PERMISSION_CODE)
+                } else{
+                    takePhoto()
+                }
+            }else{
+                takePhoto()
+            }
+
+            /*
+            val intent = Intent()
+            intent.putExtra("data", path.toURI())
+            setResult(RESULT_OK, intent)
+            */
+
+            // startActivityForResult(takePhotoIntent, IMAGE_CHOOSE)
+
+            takePhoto()
         }
 
         buttonChoose.setOnClickListener {
@@ -76,53 +96,46 @@ class MainActivity : AppCompatActivity() {
 
         // now set returned text to textview
         textView.text = obj.toString()
-
-        imageView = findViewById(R.id.imgView)
     }
 
     // companion object
     companion object {
         private val IMAGE_CHOOSE = 1000
+        private val PROCESS_IMAGE = 1002
         private val PERMISSION_CODE = 1001
     }
 
     private fun getPhotoFile(fileName: String): File {
-        /* val values = ContentValues(1)
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg")
-        val fileUri = contentResolver
-            .insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                values)
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if(intent.resolveActivity(packageManager) != null) {
-            mCurrentPhotoPath = fileUri.toString()
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri)
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
-                    or Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
-            startActivityForResult(intent, TAKE_PHOTO_REQUEST)
-        }
-         */
         val directoryStorage = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(fileName, "driver_checker.jpg", directoryStorage)
     }
 
+    private fun takePhoto() {
+        val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        filePhoto = this.getPhotoFile(FILENAME)
+        val providerFile = FileProvider.getUriForFile(this,"com.example.driverchecker.fileprovider", filePhoto)
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+        filePhotoUri = FileProvider.getUriForFile(this,"com.example.driverchecker.fileprovider", filePhoto)
+
+        imageView.setImageURI(filePhotoUri)
+        /*
+        takePhotoIntent.putExtra("data", providerFile);
+        setResult(RESULT_OK,takePhotoIntent);
+        finish()
+         */
+        startActivity(takePhotoIntent)
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        /** val REQUEST_CODE = 13
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
-             val takenPhoto = BitmapFactory.decodeFile(filePhoto.absolutePath)
-            imageView.setImageBitmap(takenPhoto)
-        } else if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
-            imageView.setImageURI(data?.data)
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                IMAGE_CHOOSE -> if (data?.data !== null) filePhotoUri = data?.data!!
+             }
         }
-        **/
-        if(requestCode == IMAGE_CHOOSE && resultCode == Activity.RESULT_OK){
-            imageView.setImageURI(data?.data)
-        }
+
+        imageView.setImageURI(filePhotoUri)
     }
-    // private const val REQUEST_CODE = 13
-    //you can type a random number, this is not important
 
     private fun chooseImageGallery() {
         val intent = Intent(Intent.ACTION_PICK)
