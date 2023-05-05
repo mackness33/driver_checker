6f4930dc8522a7cfa301159722ceef7d11578218
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-05-05 20:10:00 +0200

    Make a photo without showing the camera

--- app/src/main/java/com/example/driverchecker/MainActivity.kt
+++ app/src/main/java/com/example/driverchecker/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.driverchecker
 
+import PhotoHandler
 import android.Manifest.permission.CAMERA
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.hardware.Camera
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -19,29 +22,35 @@ import androidx.core.content.FileProvider
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
+import com.example.driverchecker.R.id.btnTake
 import com.google.android.material.snackbar.Snackbar
 import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var filePhoto: File
     private lateinit var textView: TextView
-    private lateinit var buttonView: Button
+    private lateinit var buttonTakeWithCamera: Button
+    private lateinit var buttonTake: Button
     private lateinit var buttonChoose: Button
     private lateinit var imageView: ImageView
     private lateinit var filePhotoUri: Uri
     private val FILENAME = "driver_checker.jpg"
+    private var camera: Camera? = null
+    private var cameraId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.txtView)
-        buttonView = findViewById(R.id.btnView)
+        buttonTakeWithCamera = findViewById(R.id.btnView)
+        buttonTake = findViewById(btnTake)
         buttonChoose = findViewById(R.id.btnChoose)
         imageView = findViewById(R.id.imgView)
 
-        buttonView.setOnClickListener {
+        buttonTakeWithCamera.setOnClickListener {
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
 
@@ -49,11 +58,13 @@ class MainActivity : AppCompatActivity() {
                     val permissions = arrayOf(CAMERA)
                     requestPermissions(permissions, PERMISSION_CODE)
                 } else{
-                    takePhoto()
+                    takePhotoWithCamera()
                 }
             }else{
-                takePhoto()
+                takePhotoWithCamera()
             }
+
+
 
             /*
             val intent = Intent()
@@ -63,7 +74,12 @@ class MainActivity : AppCompatActivity() {
 
             // startActivityForResult(takePhotoIntent, IMAGE_CHOOSE)
 
-            takePhoto()
+            takePhotoWithCamera()
+        }
+
+        buttonTakeWithCamera.setOnClickListener {
+            camera!!.startPreview();
+            camera!!.takePicture(null, null, PhotoHandler(getApplicationContext()));
         }
 
         buttonChoose.setOnClickListener {
@@ -76,6 +92,23 @@ class MainActivity : AppCompatActivity() {
                 }
             }else{
                 chooseImageGallery()
+            }
+        }
+
+        // do we have a camera?
+        if (!getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
+            Toast.makeText(this, "No camera on this device", Toast.LENGTH_LONG)
+                .show();
+        } else {
+            cameraId = findFrontFacingCamera();
+            if (cameraId < 0) {
+                Toast.makeText(this, "No front facing camera found.",
+                    Toast.LENGTH_LONG).show();
+            } else {
+                if ( camera != null) {
+                    camera = Camera.open(cameraId);
+                }
             }
         }
 
@@ -101,13 +134,29 @@ class MainActivity : AppCompatActivity() {
     // companion object
     companion object {
         private val IMAGE_CHOOSE = 1000
-        private val PROCESS_IMAGE = 1002
         private val PERMISSION_CODE = 1001
+        internal val DEBUG_TAG = "MainActivity"
     }
 
     private fun getPhotoFile(fileName: String): File {
         val directoryStorage = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(fileName, "driver_checker.jpg", directoryStorage)
+    }
+
+    private fun takePhotoWithCamera() {
+        val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        filePhoto = this.getPhotoFile(FILENAME)
+        val providerFile = FileProvider.getUriForFile(this,"com.example.driverchecker.fileprovider", filePhoto)
+        takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+        filePhotoUri = FileProvider.getUriForFile(this,"com.example.driverchecker.fileprovider", filePhoto)
+
+        imageView.setImageURI(filePhotoUri)
+        /*
+        takePhotoIntent.putExtra("data", providerFile);
+        setResult(RESULT_OK,takePhotoYIntent);
+        finish()
+         */
+        startActivity(takePhotoIntent)
     }
 
     private fun takePhoto() {
@@ -120,7 +169,7 @@ class MainActivity : AppCompatActivity() {
         imageView.setImageURI(filePhotoUri)
         /*
         takePhotoIntent.putExtra("data", providerFile);
-        setResult(RESULT_OK,takePhotoIntent);
+        setResult(RESULT_OK,takePhotoYIntent);
         finish()
          */
         startActivity(takePhotoIntent)
@@ -162,5 +211,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun findFrontFacingCamera(): Int {
+        var cameraId = -1
+        // Search for the front facing camera
+        val numberOfCameras: Int = Camera.getNumberOfCameras()
+        for (i in 0 until numberOfCameras) {
+            val info = Camera.CameraInfo()
+            Camera.getCameraInfo(i, info)
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                Log.d(DEBUG_TAG, "Camera found")
+                cameraId = i
+                break
+            }
+        }
+        return cameraId
+    }
+
+    override fun onPause() {
+        if (camera != null) {
+            camera!!.release()
+            camera = null
+        }
+        super.onPause()
     }
 }
