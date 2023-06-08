9f523596daf5186f9897c1b98139d5868a22f878
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-06-08 04:16:16 +0200

    Add of resultView to display boxes

--- app/src/main/java/com/example/driverchecker/MLTaskInterface.kt
+++
@@ -1,8 +0,0 @@
-package com.example.driverchecker
-
-import java.util.concurrent.Callable
-
-interface MLTaskInterface <Model, Data, Result> : Callable<Result> {
-    val mlModel: Model
-    abstract fun processAndOEvaluate(input: Data): Result?
-}
