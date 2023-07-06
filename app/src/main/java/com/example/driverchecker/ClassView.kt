42417ecea6c496beba705085ba8a2be16992e458
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-07-06 18:15:51 +0200

    Add of class view and adapter for change of color

---
+++ app/src/main/java/com/example/driverchecker/ClassView.kt
@@ -0,0 +1,62 @@
+package com.example.driverchecker
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+// Copyright (c) 2020 Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+class ClassView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+    private var paint: Paint
+    var size: Pair<Float, Float>
+        private set
+    var offset: Pair<Float, Float>
+        private set
+    var color: Int?
+        private set
+    var isActive: Boolean
+        private set
+
+
+    init {
+        paint = Paint()
+        size = Pair(0.0f, 0.0f)
+        offset = Pair(0.0f, 0.0f)
+        color = Color.BLUE
+        isActive = false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        if (isActive && size.first > 0 && size.second > 0) {
+            canvas.drawRect(
+                offset.first,
+                offset.second,
+                offset.first * size.first,
+                offset.second * size.second,
+                paint
+            )
+        }
+    }
+
+    fun updateSize (newSize: Pair<Float, Float>) {
+        size = newSize
+    }
+
+    fun updateOffset (newOffset: Pair<Float, Float>) {
+        offset = newOffset
+    }
+
+    fun updateColors(newColor: Int) {
+        color = newColor
+    }
+
+    fun activate (showColor: Boolean) {
+        isActive = showColor
+    }
+}
---
+++ app/src/main/java/com/example/driverchecker/ItemColorsAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.driverchecker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+// items are a list of map with keys the number of the superclass and as value a list of all the classes found
+
+// this adapter is actually the itemColorRecyclerView of the item in predictionAdapter.
+// PredictionAdapter display a list of "lines" which are made of various things including the itemColorRecyclerView
+//   made of all the classView
+class ItemColorsAdapter(
+    private val items: List<Boolean>,
+    private var sizeHolder: Pair<Int, Int> = Pair(120, 64),
+    private val superclass: Int = 1
+) : ColoredAdapter<ItemColorsAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val colorView: ClassView = view.findViewById(R.id.colorItem)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.color_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.itemView.layoutParams = ViewGroup.LayoutParams(sizeHolder.first, sizeHolder.second)
+        viewHolder.colorView.updateSize(
+            Pair(viewHolder.itemView.layoutParams.width.toFloat(), viewHolder.itemView.layoutParams.height.toFloat())
+        )
+        viewHolder.colorView.updateColors(colorManager.listFullColors[superclass].scale[position])
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = items.size
+}
