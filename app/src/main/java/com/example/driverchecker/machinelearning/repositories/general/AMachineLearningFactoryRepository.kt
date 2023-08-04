6fe2da024083e5056ae13adfaafad784e9b314b9
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-08-04 20:44:48 +0200

    New repositories as the abstract version of the base machinelearning one

---
+++ app/src/main/java/com/example/driverchecker/machinelearning/repositories/general/AMachineLearningFactoryRepository.kt
@@ -0,0 +1,16 @@
+package com.example.driverchecker.machinelearning.repositories.general
+
+import com.example.driverchecker.machinelearning.data.*
+import com.example.driverchecker.machinelearning.repositories.IMachineLearningFactory
+
+abstract class AMachineLearningFactoryRepository<Data, Result : WithConfidence>
+    : AMachineLearningRepository<Data, Result>, IMachineLearningFactory<Data, Result> {
+
+    constructor() : super(null)
+
+    constructor(modelName: String, modelInit: Map<String, Any?>) : super(null){
+        initUseRepo(modelName, modelInit)
+    }
+
+    protected fun initUseRepo (modelName: String, modelInit: Map<String, Any?>) = use (modelName, modelInit)
+}
