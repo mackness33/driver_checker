1e457e6d3d9fa1b01022de4db1e211460dd29f17
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-09-05 17:25:55 +0200

    New Adapter for the metric table in the result fragment

---
+++ app/src/main/java/com/example/driverchecker/ui/adapters/MetricsTableAdapter.kt
@@ -0,0 +1,74 @@
+package com.example.driverchecker.ui.adapters
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.driverchecker.R
+import com.example.driverchecker.ui.views.RectView
+import kotlin.math.round
+import kotlin.math.sqrt
+
+
+// items are a list of map with keys the number of the superclass and as value a list of all the classes found
+class MetricsTableAdapter(
+    private val items: List<Map<String, Set<Int>>>,
+    maxClasses:Int = 2,
+    private var sizeHolder: Int = 50,
+) : ColoredAdapter<MetricsTableAdapter.ViewHolder>() {
+    private val dimension: Int
+    private var colorList: Set<String>? = null
+
+    init {
+        val square = round(sqrt(maxClasses.toDouble()))
+        dimension = when {
+            maxClasses <= 0 -> 0
+            maxClasses == 1 -> 1
+            maxClasses < 4 -> 2
+            maxClasses % square > 0 -> square+1
+            else -> square
+        }.toInt()
+    }
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder)
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val predictionView: RectView = view.findViewById(R.id.rect_item)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.partial_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.predictionView.updateDimensions(Pair(dimension, dimension))
+        viewHolder.itemView.layoutParams = ViewGroup.LayoutParams(sizeHolder, sizeHolder)
+        viewHolder.predictionView.updateSize(
+            Pair(viewHolder.itemView.layoutParams.width, viewHolder.itemView.layoutParams.height)
+        )
+
+        val group = items[position].toList().first()
+        val indexColorGroup = colorList?.indexOfFirst { it.contentEquals(group.first) }
+        viewHolder.predictionView.updateColors(colorManager.listFullColors[indexColorGroup ?: 0])
+        viewHolder.predictionView.updateSelectedClasses(group.second.toList())
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = items.size
+
+    fun updateGroupList(groups: Set<String>) {
+        colorList = groups
+    }
+}
