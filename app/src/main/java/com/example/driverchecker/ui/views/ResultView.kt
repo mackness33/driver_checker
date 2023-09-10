2580221e09efa1fe16b09fd646b416f0e8e77c4d
Lucy Sarah Dumphy <david.mack@studio.unibo.it>
2023-09-10 17:24:54 +0200

    Resize the rectangle of ResultView to cover the whole view

--- app/src/main/java/com/example/driverchecker/ui/views/ResultView.kt
+++ app/src/main/java/com/example/driverchecker/ui/views/ResultView.kt
@@ -13,19 +13,18 @@ import com.example.driverchecker.machinelearning.data.IImageDetectionOutput
 // This source code is licensed under the BSD-style license found in the
 // LICENSE file in the root directory of this source tree.
 class ResultView : View {
-    private var paintRectangle: Paint? = null
-    private var paintText: Paint? = null
-    private var path: Path? = null
-    private var rect: RectF? = null
+    private var paintRectangle: Paint = Paint()
+    private var paintText: Paint = Paint()
+    private var path: Path = Path()
+    private var rectPath: RectF = RectF()
+    private var resizedRect: RectF = RectF()
     private var results: IImageDetectionOutput<String>? = null
 
-    constructor(context: Context?) : super(context) {}
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-        paintRectangle = Paint()
-        paintRectangle!!.color = Color.GREEN
-        paintText = Paint()
-        path = Path()
-        rect = RectF()
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    init {
+        paintRectangle.color = Color.GREEN
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -35,46 +34,55 @@ class ResultView : View {
         val res: IImageDetectionOutput<String> = results!!
 
         for (item: IImageDetectionItem<String> in res.listItems) {
-            drawBox(canvas, item.rect)
-            drawPath(canvas, item)
+            resizedRect.set(
+                item.rect.left * (width / 640) - paintRectangle.strokeWidth,
+                item.rect.top * (height / 640) - paintRectangle.strokeWidth,
+                item.rect.right * (width / 640) - paintRectangle.strokeWidth,
+                item.rect.bottom * (height / 640) - paintRectangle.strokeWidth,
+            )
+
+            drawBox(canvas)
+            drawPath(canvas)
             drawText(canvas, item)
         }
     }
 
     private fun drawText (canvas: Canvas, item: IImageDetectionItem<String>) {
-        paintText!!.color = Color.WHITE
-        paintText!!.strokeWidth = 0f
-        paintText!!.style = Paint.Style.FILL
-        paintText!!.textSize = 32f
+        paintText.color = Color.WHITE
+        paintText.strokeWidth = 0f
+        paintText.style = Paint.Style.FILL
+        paintText.textSize = 32f
         canvas.drawText(
             String.format(
                 "%s %.2f",
                 item.classification.name,
                 item.confidence
             ),
-            item.rect.left + TEXT_X,
-            item.rect.top + TEXT_Y,
-            paintText!!
+            resizedRect.left + TEXT_X,
+            resizedRect.top + TEXT_Y,
+            paintText
         )
     }
 
-    private fun drawPath (canvas: Canvas, box: IImageDetectionItem<String>) {
-        path?.reset()
-        rect?.set(
-            box.rect.left,
-            box.rect.top,
-            box.rect.left + TEXT_WIDTH,
-            box.rect.top + TEXT_HEIGHT
+    private fun drawPath (canvas: Canvas) {
+        path.reset()
+        rectPath.set(
+            resizedRect.left,
+            resizedRect.top,
+            resizedRect.left + TEXT_WIDTH,
+            resizedRect.top + TEXT_HEIGHT
         )
-        path?.addRect(rect!!, Path.Direction.CW)
-        paintText!!.color = Color.MAGENTA
-        canvas.drawPath(path!!, paintText!!)
+        path.addRect(rectPath, Path.Direction.CW)
+        paintText.color = Color.MAGENTA
+        canvas.drawPath(path, paintText)
     }
 
-    private fun drawBox (canvas: Canvas, rect: RectF) {
-        paintRectangle!!.strokeWidth = 5f
-        paintRectangle!!.style = Paint.Style.STROKE
-        canvas.drawRect(rect, paintRectangle!!)
+    private fun drawBox (canvas: Canvas) {
+        paintRectangle.strokeWidth = 5f
+        paintRectangle.style = Paint.Style.STROKE
+        // not working the layoutParams is always at 0. Might need to check the parameter of the layout or holder
+
+        canvas.drawRect(resizedRect, paintRectangle)
     }
 
     fun setResults(results: IImageDetectionOutput<String>?) {
